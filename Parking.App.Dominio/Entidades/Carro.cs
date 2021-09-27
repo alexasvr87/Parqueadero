2224959239dacd1074da5510292c47bdb4e5b944
Alexandra Vargas <alexasvr@gmail.com>
2021-09-27 03:36:37 -0500

    Primera Version

--- Parking.App.Dominio/Entidades/Carro.cs
+++ Parking.App.Dominio/Entidades/Carro.cs
@@ -3,10 +3,6 @@ namespace Parking.App.Dominio
 {
     public class Carro : Vehiculo
     {
-        public string Placa { get; set; }
-        public string Marca { get; set; }
-        public string Modelo { get; set; }
-        public string Color { get; set; }
-        public TipoVehiculo TipoVehiculo { get; set; }
+
     }
 }
