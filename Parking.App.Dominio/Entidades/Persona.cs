05ab1450a0c71744ac2b707bf0133418de5b10c4
Alexandra Vargas <alexasvr@gmail.com>
2021-09-27 03:31:03 -0500

    Primera Version

--- Parking.App.Dominio/Entidades/Persona.cs
+++ Parking.App.Dominio/Entidades/Persona.cs
@@ -3,10 +3,10 @@ namespace Parking.App.Dominio
 {
     public class Persona
     {
-        public int Id { get; set;}
-        public string Usuario { get; set;}
-        private string Contraseña { get; set;}
-        public string Nombres { get; set;}
-        public string Apellidos { get; set;}
+        public int Id { get; set; }
+        public string Usuario { get; set; }
+        private string Password { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
     }
 }
