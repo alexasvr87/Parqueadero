8b28ec4ed875c773ce16a217a72702286695d53d
Alexandra Vargas <alexasvr@gmail.com>
2021-10-01 18:27:44 -0500

    nueva version

--- Parking.App.Consola/Program.cs
+++ Parking.App.Consola/Program.cs
@@ -9,16 +9,24 @@ namespace Parking.App.Consola
         {
             Cliente objCliente1 = new Cliente()
             {
-                Id = 7777777,
                 Usuario = "usuario007",
                 Password = "usuario7777777",
                 Nombres = "Homero Jay",
                 Apellidos = "Simpson",
                 Direccion = "Calle 100 # 11b - 67",
                 Telefono = "1234567",
+                FechaNacimiento = "19/04/1987",
+                Vehiculo = objVehiculo1
+            };
 
+            Vehiculo objVehiculo1 = new Vehiculo()
+            {
+                Placa = "JNV78",
+                Marca = "AKT",
+                Modelo = "Jet5",
+                Color = "Negro",
+                TipoVehiculo = Moto,
             };
         }
-
     }
 }
